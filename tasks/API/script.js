ae465f2e0c8db0bf5e59537d847c7a80b914dd25
Yojana <yojanapandey29@gmail.com>
2024-08-28 15:29:25 +0545

    search added

--- tasks/API/script.js
+++ tasks/API/script.js
@@ -1,5 +1,6 @@
 
 const container = document.querySelector(".container");
+const search = document.querySelector("#enter");
 //fetch url
 const BaseUrl = "https://restcountries.com/v3.1/all";
 
@@ -13,6 +14,8 @@ fetch(BaseUrl).then((res)=>{
     console.log("error occured",err);
 });
 function displayCountries(data){
+    
+    container.innerHTML = '';
 
     for(let i = 0;i<data.length;i++){
         // console.log(data);
@@ -21,18 +24,33 @@ function displayCountries(data){
 
         countryDiv.innerHTML=`
         <div>
-        <p>SN:${i}</p>
         <img src=${data[i].flags.png} alt="${data[i].flags.alt}">
         <p>Name:${data[i].name.common}</p>
         <p>Capital:${data[i].capital}</p>
         <p>Continent:${data[i].continents}</p>
+        <p>Population:${data[i].population}</p>
         </div>
         `
 
         container.append(countryDiv);
     }
-
-
-
 }
+
+
+search.addEventListener("input",function(){
+    const value = this.value;
+    // console.log(value);
+    const searchUrl = `https://restcountries.com/v3.1/name/${value}`;
+
+    fetch(searchUrl).then((res)=>{
+       return res.json();
+    }).then(country => {
+        console.log(country);
+        displayCountries(country);
+    }).
+catch(error=>{
+    console.log("error occurred",error.message);
+});
+   
+});
 
