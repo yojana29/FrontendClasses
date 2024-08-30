0bab984887196a56838e4d53c8a6a04675946205
Yojana <yojanapandey29@gmail.com>
2024-08-30 22:41:25 +0545

    updating

--- tasks/API/script.js
+++ tasks/API/script.js
@@ -8,27 +8,27 @@ cross = document.querySelector("#cross");
 //fetch url
 const BaseUrl = "https://restcountries.com/v3.1/all";
 
-fetch(BaseUrl).then((res)=>{
+fetch(BaseUrl).then((res) => {
     return res.json();
-}).then((data)=>{
+}).then((data) => {
     console.log(data);
     displayCountries(data);
-    
-}).catch((err)=>{
-    console.log("error occured",err);
+
+}).catch((err) => {
+    console.log("error occured", err);
 });
 
 //display data
-function displayCountries(data){
-    
+function displayCountries(data) {
+
     container.innerHTML = '';
 
-    for(let i = 0;i<data.length;i++){
+    for (let i = 0; i < data.length; i++) {
         // console.log(data);
         let countryDiv = document.createElement("div");
         countryDiv.classList.add("country");
 
-        countryDiv.innerHTML=`
+        countryDiv.innerHTML = `
         <div>
         <img src=${data[i].flags.png} alt="${data[i].flags.alt}">
         <p>Name:${data[i].name.common}</p>
@@ -44,51 +44,51 @@ function displayCountries(data){
 
 //search data
 
-search.addEventListener("input",function(){
+search.addEventListener("input", function () {
     const value = this.value;
     // console.log(value);
-    let searchUrl=`https://restcountries.com/v3.1/name/${value}`;
+    let searchUrl = `https://restcountries.com/v3.1/name/${value}`;
 
-  
 
-    fetch(searchUrl).then((res)=>{
-       return res.json();
+
+    fetch(searchUrl).then((res) => {
+        return res.json();
     }).then(country => {
         console.log(country);
         displayCountries(country);
     }).
-catch(error=>{
-    console.log("error occurred",error.message);
-});
+        catch(error => {
+            console.log("error occurred", error.message);
+        });
 
 
 
-   
+
 });
 
 //clear search
 
-cross.addEventListener("click",()=>{
+cross.addEventListener("click", () => {
 
     search.value = "";
     const BaseUrl = "https://restcountries.com/v3.1/all";
 
-fetch(BaseUrl).then((res)=>{
-    return res.json();
-}).then((data)=>{
-    console.log(data);
-    displayCountries(data);
-    
-}).catch((err)=>{
-    console.log("error occured",err);
-});
+    fetch(BaseUrl).then((res) => {
+        return res.json();
+    }).then((data) => {
+        console.log(data);
+        displayCountries(data);
+
+    }).catch((err) => {
+        console.log("error occured", err);
+    });
 })
 
 
 //filtering
-filter.addEventListener("change",()=>{
+filter.addEventListener("change", () => {
     region = filter.value;
-     let filterUrl = `https://restcountries.com/v3.1/region/${region}`;
+    let filterUrl = `https://restcountries.com/v3.1/region/${region}`;
 
     if (region === "All") {
         // If select by Region is selected or if you want to show all countries
@@ -98,12 +98,12 @@ filter.addEventListener("change",()=>{
         filterUrl = `https://restcountries.com/v3.1/region/${region}`;
     }
 
-    fetch(filterUrl).then(res=>{
-       return res.json();
-    }).then(area=>{
+    fetch(filterUrl).then(res => {
+        return res.json();
+    }).then(area => {
         console.log(area);
         displayCountries(area);
-    }).catch(error=>{
+    }).catch(error => {
         console.log(error.message);
     });
 });
