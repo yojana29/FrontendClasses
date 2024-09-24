47ebeeb10f7aa46173b85d37797fc4572e92a9b0
yojana29 <yojanapandey29@gmail.com>
2024-09-24 22:13:10 +0545

    added

---
+++ practice/src/App.jsx
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+import './App.css';
+
+
+function App() {
+  
+
+  return (
+
+   <>
+   <div>This is a homepage.</div>
+   <a href = "/about-us">About Us</a>
+
+   </> 
+  
+  )
+}
+
+export default App;
---
+++ practice/src/components/about-us.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const AboutUs = () => {
+  return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat dicta provident commodi eum velit! Itaque expedita ipsam ratione sequi.
+             Accusamus impedit exercitationem molestiae eaque vitae iusto libero numquam eligendi pariatur.
+        </div>
+  )
+}
+export default AboutUs;
