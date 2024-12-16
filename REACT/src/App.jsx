54dca4d93e79805ea4cab65c6a37309dcbd376e5
yojana29 <yojanapandey29@gmail.com>
2024-12-16 22:00:58 +0545

    working on it

---
+++ REACT/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="App">
+      <h1>To Do List</h1>
+
+
+     </div>
+       
+    </>
+  )
+}
+
+export default App
