670708278764fbdf5160eb2657a38517eac343eb
yojana29 <yojanapandey29@gmail.com>
2024-12-18 21:29:02 +0545

    working on it

--- REACT/src/App.jsx
+++ REACT/src/App.jsx
@@ -10,6 +10,23 @@ function App() {
     <>
      <div className="App">
       <h1>To Do List</h1>
+      <div className = "todo-wrapper">
+        <div className="todo-input">
+          <div className="title">
+          <label> Title</label>
+          <input type="text" placeholder="what's the title" />
+          </div>
+          <div className="description">
+            <label>Description</label>
+            <input type="text" placeholder='describe your task' />
+          </div>
+          <div className="button">
+            <button className="add">Add</button>
+          </div>
+          
+        </div>
+
+      </div>
 
 
      </div>
